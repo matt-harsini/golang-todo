b88a9f4596af9e46ec9293170407996ca0798f68
Matthew Kim <matthewkimcs@gmail.com>
2023-09-28 18:29:27 -0700

    refactoring file structure and code organization;

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -1,14 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 type application struct {
 }
 
 func main() {
+	
 	app := &application{}
 	server := &http.Server{
 		Handler: app.routes(),
 	}
 	server.ListenAndServe()
+}
+
+func openDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "golang:pass@/golangtodo?parseTime=true")
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
